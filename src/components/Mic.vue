91b40b2c4014c8a7f777c09ee7be8291cda18971
RetroPronghorn <retropronghorn@gmail.com>
2020-09-23 17:32:41 -0400

    fix(RoomCode): Add room code

--- src/components/Mic.vue
+++ src/components/Mic.vue
@@ -1,17 +1,17 @@
 <template>
     <div v-if="peer && peer.open">
       <h3 id="mic" class="pulse-button" v-if="!muted" v-on:click="togglemute">
-        <i class="fa fa-microphone fa-hover-hidden"></i>
-        <i class="fa fa-microphone-slash fa-hover-show" style="margin-left: -3px;"></i>
+        <i class="fas fa-microphone-alt fa-hover-hidden"></i>
+        <i class="fa fa-microphone-alt-slash fa-hover-show" style="margin-left: -11.4px;"></i>
       </h3>
       <h3 id="mic" class="pulse-button pulse-button-red" v-else v-on:click="togglemute">
-        <i class="fa fa-microphone fa-hover-show red"></i>
-        <i class="fa fa-microphone-slash fa-hover-hidden red" style="margin-left: -3px;"></i>
+        <i class="fas fa-microphone-alt fa-hover-show red"></i>
+        <i class="fas fa-microphone-alt-slash fa-hover-hidden red" style="margin-left: -11.4px;"></i>
       </h3>
     </div>
     <div v-else>
       <h3 id="mic" class="pulse-button" style="box-shadow:none;">
-        <i class="fa fa-refresh fa-spin" style="color:#fff;"></i>
+        <i class="fas fa-circle-notch fa-spin" style="margin-left: -5.4px; color: #fff;"></i>
       </h3>
     </div>
 </template>
@@ -50,6 +50,7 @@ export default {
     font-size: 40px;
     margin: 0;
     color: #fff;
+    text-align: center;
   }
   * > .fa-hover-show,
   *:hover > .fa-hover-hidden {
