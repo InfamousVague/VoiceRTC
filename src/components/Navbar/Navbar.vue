985a3bb3ababc616d180977df88b2e106fc0e422
RetroPronghorn <retropronghorn@gmail.com>
2020-09-25 01:27:29 -0400

    feat(Output): Output switching through setSink

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="navbar">
     <h3>VoiceRTC <span class="small"><span style="color:red;">BETA</span> by RetroPronghorn</span></h3>
-    <span class="nav-button">
+    <span class="nav-button" v-on:click="minimize">
         <i class="icon icon-minus" id="minimize"></i>
     </span>
-    <span class="nav-button">
+    <span class="nav-button" v-on:click="close">
         <i class="nav-button icon icon-cross" id="maximize"></i>
     </span>
   </div>
